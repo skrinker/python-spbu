623e1ffc0614db2360d817d55c06ef816b549edd
skrinker <guzel28sit@gmail.com>
2022-03-25 14:26:36 +0300

    tests & task

---
+++ kr_1/kr1_1.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from functools import update_wrapper
+from typing import Callable, List, Any, Tuple
+from typing import Optional, Callable, Any
+
+
+class Spy:
+    def __init__(self, function: Callable):
+        self._function = function
+        self.logs: List[Tuple] = []
+        update_wrapper(self, function)
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        current = datetime.now()
+        params = {"args": args, "kwargs": kwargs}
+        self.logs.append((current, params))
+
+        return self._function(*args, **kwargs)
+
+
+def print_usage_statistic(function: Callable):
+    if isinstance(function, Spy):
+        for log in function.logs:
+            yield log
+        return
+
+    raise ValueError("The function should be decorated with @Spy.")
+
---
+++ tests/kr1/kr1_1.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest import TestCase
+
+from kr_1.kr1_1 import print_usage_statistic, Spy
+
+
+def check_message(actual, expected: str) -> bool:
+    return expected in str(actual.exception)
+
+
+class SpyTestCase(unittest.TestCase):
+    def test_error_function(self):
+        with self.assertRaises(ValueError) as context:
+            def foo():
+                return 5
+
+            for _, _ in print_usage_statistic(foo):
+                pass
+
+        self.assertTrue(check_message(context, "The function should be decorated with @Spy."))
+
+    def test_usual_function(self):
+        @Spy
+        def foo(number): return number
+
+        foo(1)
+        foo(2)
+        foo(3)
+
+        actual = []
+        for _, parameters in print_usage_statistic(foo):
+            actual.append(parameters)
+
+
+        expected = [
+            {"args": (1,), "kwargs": {}},
+            {"args": (2,), "kwargs": {}},
+            {"args": (3,), "kwargs": {}}
+
+        ]
+
+        self.assertEqual(actual, expected)
